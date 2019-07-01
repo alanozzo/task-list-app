5f85cbffb214a49cecc772c9570e29a1a0a89145
Alan Emmanuel Meza González <alan_emmanuel_meza@hotmail.com>
2019-07-01 00:08:14 -0500

    REST server-usaurio

---
+++ server/routes/usuario.js
@@ -0,0 +1,123 @@
+const express = require('express');
+const bcrypt = require('bcrypt');
+const app = express();
+const _ = require('underscore');
+const Usuario = require('../models/usuario');
+
+
+app.get('/usuario', function(req, res) {
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+
+    let limite = req.query.limite || 0;
+    limite = Number(limite);
+
+    Usuario.find({ estado: true }, 'nombre email role estado google img').skip(desde).limit(limite).exec((error, usuarios) => {
+        if (error) {
+            return res.status(400).json({
+                ok: false,
+                error
+            });
+        }
+
+        Usuario.count({ estado: true }, (error, conteo) => {
+            res.json({
+                ok: true,
+                usuarios,
+                cuanros: conteo
+            });
+        });
+    });
+});
+
+app.post('/usuario', function(req, res) {
+
+    let body = req.body;
+    let usuario = new Usuario({
+        nombre: body.nombre,
+        email: body.email,
+        password: bcrypt.hashSync(body.password, 10),
+        role: body.role
+    });
+
+    usuario.save((error, usuarioDB) => {
+        if (error) {
+            return res.status(400).json({
+                ok: false,
+                error
+            });
+        }
+        // usuarioDB.password = null;
+
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    });
+});
+
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    //we just pick values we want to update, not password nor google status
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (error, usuarioDB) => {
+        if (error) {
+            return res.status(400).json({
+                ok: false,
+                error
+            });
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    });
+});
+
+app.put('/updateStatusUsuario/:id', function(req, res) {
+    let id = req.params.id;
+    //we just pick values we want to update, not password nor google status
+    let body = _.pick(req.body, ['estado']);
+
+    Usuario.findByIdAndUpdate(id, body, { new: true }, (error, usuarioDB) => {
+        if (error) {
+            return res.status(400).json({
+                ok: false,
+                error
+            });
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        });
+    });
+});
+
+app.delete('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+
+    // Usuario.findByIdAndRemove(id, (error, usuarioBorrado) => {
+    Usuario.findByIdAndUpdate(id, { estado: false }, { new: true }, (error, usuarioBorrado) => {
+        if (error) {
+            return res.status(400).json({
+                ok: false,
+                error
+            });
+        }
+        if (!usuarioBorrado) {
+            return res.status(400).json({
+                ok: false,
+                error: {
+                    message: 'Usuario no encontrado'
+                }
+            });
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioBorrado
+        });
+    });
+});
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,43 +1,27 @@
 require('./config/config');
 const express = require('express');
 const app = express();
+
+// Using Node.js `require()`
+const mongoose = require('mongoose');
+
+
 const bodyParser = require('body-parser');
 // parse application/x-www-form-urlencoded ---this is a middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json ---this is a middleware
 app.use(bodyParser.json());
+app.use(require('./routes/usuario'));
 
+mongoose.connect(process.env.URLDB, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true
+}, (error, response) => {
+    if (error) throw error;
 
-
-app.get('/usuario', function(req, res) {
-    res.json('get usuario')
-});
-
-app.post('/usuario', function(req, res) {
-
-    let body = req.body;
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona: body
-        });
-    }
-});
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario')
+    console.log('Base de datos ONLINE');
 });
 
 app.listen(process.env.PORT, () => {
